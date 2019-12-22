61948015ee4957d25d08841faf64c8126f7b3602
Radu <radu.pop@connatix.com>
2019-12-22 17:59:56 +0200

    initial commit

---
+++ Galaxy.Planets.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Galaxy.Planets.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Galaxy.Planets.Presentation/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using NLog.Web;
+
+namespace Galaxy.Planets.Presentation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logConfig = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ENVIRONMENT"))
+                ? "NLog.Development.config"
+                : "NLog.config";
+            var logger = NLogBuilder.ConfigureNLog(logConfig).GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:5006", "https://*:5007")
+                        .UseStartup<Startup>();
+                });
+    }
+}
